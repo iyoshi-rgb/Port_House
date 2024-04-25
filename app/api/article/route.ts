ba5e4191b8f5f70ccc05fa7d5f27de673385424e
yoshito <yoshito.i.2002@gmail.com>
2024-04-25 22:29:25 +0900

    ###

--- app/api/article/route.ts
+++ app/api/article/route.ts
@@ -3,18 +3,18 @@ import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export default async function main(){
+export const connect = async () => {
     try {
-        await prisma.$connect()
-    }catch(err){
-        return Error('DB接続失敗');
+        //prismaでデータベースに接続
+        prisma.$connect();
+    } catch (error) {
+        return Error("DB接続失敗しました")
     }
-    
 }
 
 export const GET = async (req: Request, res: NextResponse) => {
     try {
-        await main();
+        await connect();
         const articles = await prisma.article.findMany();
         return NextResponse.json({message: 'Success', articles},{status: 200});
     }catch(err){
